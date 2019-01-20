ea9d81ea70dd63cfd6fdf58a606f09f2f46c6fe4
Lee ChaeHyeon <chlee1001@naver.com>
2019-01-20 23:35:06 +0900

    Provider 컴포넌트로 리액트 앱에 store 연동

--- src/index.js
+++ src/index.js
@@ -6,8 +6,14 @@ import './index.scss';
 // 리덕스 관련 불러오기
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Provider } from 'react-redux'; // Provider는 react-redux 라이브러리에 내장된 리액트 애플리케이션에 손쉽게 스토어를 연동할 수 있도록 도와주는 컴포넌트
 
 // 스토어 생성
 const store = createStore(reducers);
 
-render(<App />, document.getElementById('root'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
