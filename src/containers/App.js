f16bd4d4a472fdf24c33eea0005513d015c8e893
Lee ChaeHyeon <chlee1001@naver.com>
2019-01-20 23:49:06 +0900

    CounterContainer 컴포넌트 생성

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Counter from '../components/Counter.js';
+import CounterContainer from './CounterContainer';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Counter />
+        <CounterContainer />
       </div>
     );
   }
