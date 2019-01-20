96f02d4c2f5c8c56856fddf992577f23b89dfec1
Lee ChaeHyeon <chlee1001@naver.com>
2019-01-20 23:32:21 +0900

    스토어 생성

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import { render } from 'react-dom';
 import App from './containers/App';
 import './index.scss';
 
+// 리덕스 관련 불러오기
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+// 스토어 생성
+const store = createStore(reducers);
+
 render(<App />, document.getElementById('root'));
