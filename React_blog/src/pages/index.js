e82d79bc1a12216de3e04f6493fb5a08aff89590
Lee ChaeHyeon <chlee1001@naver.com>
2019-02-06 23:45:20 +0900

    make blog

--- React_blog/src/pages/index.js
+++ React_blog/src/pages/index.js
@@ -1,8 +1,4 @@
-/* 다음 코드는 컴포넌트를 불러온 후 
-동일한 이름으로 내보냅니다.
-*/
-
-export { default as Home } from './Home';
-export { default as About } from './About';
-export { default as Post } from './Post';
-export { default as Posts } from './Posts';
+export { default as EditorPage } from './EditorPage';
+export { default as ListPage } from './ListPage';
+export { default as NotFoundPage } from './NotFoundPage';
+export { default as PostPage } from './PostPage';
